2466e1b50ff7becf23e065fefc30fe7df065057c
Neepan <neepanb123@gmail.com>
2024-10-14 22:48:18 +0530

    Refactor A_Two_Screens.cpp to find the longest common substring between two strings

---
+++ A_Two_Screens.cpp
@@ -0,0 +1,53 @@
+/* find the longest string which is part of
+both the strings.
+--> len(s)+len(t)+1 {for copying} +i {where i is the
+length of charcters common in both the strings}*/
+
+// Neepan Biswas
+// neepanb ~ codeforces
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fr(i, n) for (lli i = 0; i < n; i++)
+#define pb push_back
+#define sz(x) x.size()
+#define ff first
+#define ss second
+#define all(v) v.begin(), v.end()
+#define debug(x) cout << '>' << #x << ':' << x << endl;
+#define cd(condition, value_if_true, value_if_false) ((condition) ? (value_if_true) : (value_if_false))
+
+using lli = long long int;
+using ld = long double;
+using pi = pair<lli, lli>;
+using vp = vector<pi>;
+using vi = vector<lli>;
+using bl = bool;
+using mp = map<lli, lli>;
+using ump = unordered_map<lli, lli>;
+using str = string;
+
+#pragma GCC optimize("unroll-loops,O3,Ofast")
+#pragma GCC target("avx2,avx,fma,bmi,bmi2,lzcnt,popcnt")
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    lli t;
+    cin >> t;
+
+    while (t--)
+    {
+        str s,q;
+        cin>>s>>q;
+
+        lli i=0;
+        while(i<sz(s) && i<sz(q) && s[i]==q[i]){
+            i++;
+            cout<<min(static_cast<lli>(sz(s)+sz(q)), static_cast<lli>(sz(s)+sz(q)-i+1))<<endl;
+        }
+    }
+    return 0;
+}
