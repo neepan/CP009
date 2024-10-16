40a6a1c2cb68a568de0658d351f7bbe74f534a70
Neepan <neepanb123@gmail.com>
2024-10-16 22:59:51 +0530

    Refactor A_Roman_and_Browser.cpp to optimize the calculation of the maximum difference between the counts of two types of elements in an array

---
+++ A_Roman_and_Browser.cpp
@@ -0,0 +1,58 @@
+// Neepan Biswas
+// neepanb ~ codeforces
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fr(i, n) for (lli i = 0; i < n; i++)
+#define pb push_back
+#define sz(x) (lli) x.size()
+#define ff first
+#define ss second
+#define all(v) v.begin(), v.end()
+#define debug(x) cout << '>' << #x << ':' << x << endl;
+#define cd(condition, value_if_true, value_if_false) ((condition) ? (value_if_true) : (value_if_false))
+
+using lli = long long int;
+using ld = long double;
+using pi = pair<lli, lli>;
+using vp = vector<pi>;
+using vi = vector<lli>;
+using bl = bool;
+using mp = map<lli, lli>;
+using ump = unordered_map<lli, lli>;
+using str = string;
+
+#pragma GCC optimize("unroll-loops,O3,Ofast")
+#pragma GCC target("avx2,avx,fma,bmi,bmi2,lzcnt,popcnt")
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    lli n, k;
+    cin >> n >> k;
+    vi a(n);
+    fr(i, n) cin >> a[i];
+    lli max = 0;
+    fr(b, n)
+    {
+        lli e = 0, s = 0;
+        fr(i, n)
+        {
+            if ((i + 1 - b) % k != 0)
+            {
+                if (a[i] == 1)
+                    ++e;
+                else
+                    ++s;
+            }
+        }
+        lli diff = abs(e - s);
+        if (diff > max)
+            max = diff;
+    }
+    cout<<max<<endl;
+
+    return 0;
+}
