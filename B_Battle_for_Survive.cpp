6d08a1e78d37518942bae772c81f477dad86dea8
Neepan <neepanb123@gmail.com>
2025-01-15 17:36:04 +0530

    added

--- B_Battle_for_Survive.cpp
+++ B_Battle_for_Survive.cpp
@@ -1,26 +1,26 @@
 // Neepan Biswas
-// neepanb ~ codeforces
-#include<bits/stdc++.h>
+// neepan ~ codeforces
+#include <bits/stdc++.h>
 using namespace std;
 
 using lli = long long int;
 using ch = char;
-using str=string;
+using str = string;
 using ld = long double;
-using pi = pair<lli,lli>;
+using pi = pair<lli, lli>;
 using vp = vector<pi>;
 using vi = vector<lli>;
 using vs = vector<str>;
 using vc = vector<ch>;
-using bl =bool;
-using mp=map<lli,lli>;
-using ump=unordered_map<lli,lli>;
+using bl = bool;
+using mp = map<lli, lli>;
+using ump = unordered_map<lli, lli>;
 
-#define fr(i,x,n) for (lli i=(x);i<=n;i++)
-#define fr1(i,x,n) for (lli i = n; i>=(x); i--)
+#define fr(i, x, n) for (lli i = (x); i <= n; i++)
+#define fr1(i, x, n) for (lli i = n; i >= (x); i--)
 #define pb push_back
 #define eb emplace_back
-#define sz(x) (lli)x.size()
+#define sz(x) (lli) x.size()
 #define ff first
 #define ss second
 #define all(v) v.begin(), v.end()
@@ -34,44 +34,34 @@ using ump=unordered_map<lli,lli>;
 // const lli MAX_N = 1000; // Adjust based on constraints
 // vector<vector<lli>> a(MAX_N, vector<lli>(MAX_N));
 
+void neepan()
+{
+    lli n;
+    cin >> n;
+    vi a(n);
+    fr(i, 0, n - 1) cin >> a[i];
 
-void neepan(){
-	lli n,k;
-	cin>>n>>k;
-	vi a(n);
-	fr(i,0,n-1)cin>>a[i];
+    lli sum = 0;
+    fr(i,0,n-3){
+        sum+=a[i];
+    }
+     cout<<a[n-1]+sum-a[n-2]<<endl;
 
-
-	lli ans=0;
-
-	fr(i,0,n-1){
-		bl f=true;
-		for(lli j=0;j<n;j++){
-			if(i==j)continue;
-			if(abs(a[i]-a[j])%k == 0){
-				f=false;
-				break;
-			}
-		}
-		if(f){
-			cout<<"YES"<<endl;
-			cout<<i+1<<endl;
-			return;
-		}
-	}
-	cout<<"NO"<<endl;
-
+     // lli ans= accumulate(all(a),0LL) - 2*a[n-2];
+    // cout<<ans<<endl;
 }
 
-int main(){
-	ios::sync_with_stdio(false);
-	cin.tie(nullptr);
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-	lli t;
-	cin>>t;
+    lli t = 1;
+    cin >> t;
 
-	while(t--){
-		neepan();
-	}
-	return 0;
+    while (t--)
+    {
+        neepan();
+    }
+    return 0;
 }
