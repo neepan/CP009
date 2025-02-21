756135e19972fd77f4aa5a60b60f3f109f813df3
Neepan <neepanbiswas@gmail.com>
2025-02-21 17:12:21 +0530

    added

---
+++ A_Shape_Perimeter.cpp
@@ -0,0 +1,65 @@
+// Neepan Biswas
+// neepan ~ codeforces
+#include<bits/stdc++.h>
+using namespace std;
+ 
+using lli = long long int;
+using ld = long double;
+using pi = pair<lli,lli>;
+using vp = vector<pi>;
+using vi = vector<lli>;
+using mp=map<lli,lli>;
+using ump=unordered_map<lli,lli>;
+ 
+#define fr(i,x,n) for (lli i=(x);i<=(n);i++)
+#define fr1(j, n,x) for (lli j = (n); j >= (x); j--)
+#define _test   int _TEST=1; cin>>_TEST; while(_TEST--)
+#define pb push_back
+#define eb emplace_back
+#define sz(x) (lli)x.size()
+#define ff first
+#define ss second
+#define all(v) v.begin(), v.end()
+#define debug(x) cout << '>' << #x << ':' << x << endl;
+#define cd(condition, value_if_true, value_if_false) ((condition) ? (value_if_true) : (value_if_false))
+#define yes cout << "YES"<<endl
+#define no cout << "NO"<<endl
+ 
+const int inf = 9e17;
+const int mod = 1e9 + 7;
+const int NUM = 1000030;
+const int N = 10000000;
+ 
+#pragma GCC optimize("unroll-loops,O3,Ofast")
+#pragma GCC target("avx2,avx,fma,bmi,bmi2,lzcnt,popcnt")
+ 
+// Global matrix to prevent stack overflow
+// const lli MAX_N = 1000; // Adjust based on constraints
+// vector<vector<lli>> a(MAX_N, vector<lli>(MAX_N));
+ 
+void neepan(){
+ lli n,m;
+ cin>>n>>m;
+ vi x(n),y(n);
+
+ fr(i,0,n-1)cin>>x[i]>>y[i];
+
+ lli ans=0;
+ fr(i,0,n-2){
+    ans+=4*m;
+    lli w= m-x[i+1];
+    lli h=m-y[i+1];
+    ans-=2*(h+w);
+ }
+ ans+=4*m;
+ cout<<ans<<endl;
+ 
+}
+ 
+int main(){
+ios::sync_with_stdio(false);
+cin.tie(nullptr);
+ 
+_test
+neepan();
+}
