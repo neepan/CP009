313836f304e34e0d9e6b2e4754d4e72f946858d5
Neepan <neepanbiswas@gmail.com>
2025-03-07 22:13:53 +0530

    added

--- A_Make_It_Zero.cpp
+++ A_Make_It_Zero.cpp
@@ -1,71 +1,7 @@
-// Neepan Biswas
-// neepan ~ codeforces
-#include <bits/stdc++.h>
-using namespace std;
+#include <iostream>
 
-using lli = long long int;
-using ld = long double;
-using pi = pair<lli, lli>;
-using vp = vector<pi>;
-using vi = vector<lli>;
-using mp = map<lli, lli>;
-using ump = unordered_map<lli, lli>;
-
-#define fr(i, x, n) for (lli i = (x); i <= (n); i++)
-#define fr1(j, n, x) for (lli j = (n); j >= (x); j--)
-#define _test      \
-    int _TEST = 1; \
-    cin >> _TEST;  \
-    while (_TEST--)
-#define pb push_back
-#define eb emplace_back
-#define sz(x) (lli) x.size()
-#define ff first
-#define ss second
-#define all(v) v.begin(), v.end()
-#define debug(x) cout << '>' << #x << ':' << x << endl;
-#define cd(condition, value_if_true, value_if_false) ((condition) ? (value_if_true) : (value_if_false))
-
-#pragma GCC optimize("unroll-loops,O3,Ofast")
-#pragma GCC target("avx2,avx,fma,bmi,bmi2,lzcnt,popcnt")
-
-// Global matrix to prevent stack overflow
-// const lli MAX_N = 1000; // Adjust based on constraints
-// vector<vector<lli>> a(MAX_N, vector<lli>(MAX_N));
-
-void neepan()
-{
-    lli n;
-    cin >> n;
-
-    vi a(n);
-    fr(i, 0, n - 1) cin >> a[i];
-
-    vp v;
-    v.eb(1, n);
-
-    if (n & 1)
-    {
-        v.eb(1, n - 2);
-        v.eb(n - 2, n);
-        v.eb(n - 2, n);
-    }
-    else
-    {
-        v.eb(1, n);
-    }
-    cout << sz(v) << endl;
-    for (const auto &x : v)
-    {
-        cout << x.ff << ' ' << x.ss << endl;
-    }
-}
-
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    _test
-    neepan();
+int main() {
+    int x = "Hello"; // Type mismatch: assigning a string to an int
+    std::cout << x << std::endl;
+    return 0;
 }
