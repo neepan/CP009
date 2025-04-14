bb23d5f5b1cf0f130fc36e226270cdb49351b090
Neepan <neepanbiswas@gmail.com>
2025-04-14 10:03:57 +0530

    added

---
+++ C_Yarik_and_Array.cpp
@@ -0,0 +1,76 @@
+// Neepan Biswas
+// neepan ~ codeforces
+#include <bits/stdc++.h>
+using namespace std;
+
+using lli = long long int;
+using ld = long double;
+using pi = pair<lli, lli>;
+using vp = vector<pi>;
+using vi = vector<lli>;
+using mp = map<lli, lli>;
+using ump = unordered_map<lli, lli>;
+
+#define fr(i, x, n) for (lli i = (x); i <= (n); i++)
+#define fr1(j, n, x) for (lli j = (n); j >= (x); j--)
+#define _test    \
+  int _TEST = 1; \
+  cin >> _TEST;  \
+  while (_TEST--)
+#define pb push_back
+#define eb emplace_back
+#define sz(x) (lli) x.size()
+#define ff first
+#define ss second
+#define all(v) v.begin(), v.end()
+#define debug(x) cout << '>' << #x << ':' << x << endl;
+#define cd(condition, value_if_true, value_if_false) ((condition) ? (value_if_true) : (value_if_false))
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+const int inf = 9e17;
+const int mod = 1e9 + 7;
+const int NUM = 1000030;
+const int N = 10000000;
+const int MAX_N = 1e6 + 5;
+
+#pragma GCC optimize("unroll-loops,O3,Ofast")
+#pragma GCC target("avx2,avx,fma,bmi,bmi2,lzcnt,popcnt")
+
+// Global matrix to prevent stack overflow
+// const lli MAX_N = 1000; // Adjust based on constraints
+// vector<vector<lli>> a(MAX_N, vector<lli>(MAX_N));
+
+void neepan()
+{
+  lli n;
+  cin >> n;
+  vi a(n);
+  fr(i, 0, n - 1) cin >> a[i];
+
+  lli sum = 0;
+  lli ans = -1e18;
+  fr(i, 0, n - 1)
+  {
+
+    if (sum < 0 || (i != 0 && abs(a[i]) % 2 == abs(a[i - 1]) % 2))
+    {
+      // since we are looping fromt the 0th index and we are using the a[i-1] that why we are doing i!=0, which will segmentation fault
+      sum = 0;
+    }
+
+    sum += a[i];
+    ans = max(ans, sum);
+  }
+
+  cout << ans << endl;
+}
+
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  _test
+  neepan();
+}
