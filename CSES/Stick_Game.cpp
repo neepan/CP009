253796e1295177421273405e3040f17b1ba6b709
Neepan <neepanbiswas@gmail.com>
2025-04-19 19:25:42 +0530

    added

---
+++ CSES/Stick_Game.cpp
@@ -0,0 +1,75 @@
+// Neepan Biswas
+// neepan ~ codeforces
+#include <bits/stdc++.h>
+using namespace std;
+
+using lli = long long int;
+using ld = long double;
+using pi = pair<lli, lli>;
+using vp = vector<pi>;
+using vi = vector<lli>;
+using mp = map<lli, lli>;
+using ump = unordered_map<lli, lli>;
+
+#define fr(i, x, n) for (lli i = (x); i <= (n); i++)
+#define fr1(j, n, x) for (lli j = (n); j >= (x); j--)
+#define _test      \
+    int _TEST = 1; \
+    cin >> _TEST;  \
+    while (_TEST--)
+#define pb push_back
+#define eb emplace_back
+#define sz(x) (lli) x.size()
+#define ff first
+#define ss second
+#define all(v) v.begin(), v.end()
+#define debug(x) cout << '>' << #x << ':' << x << endl;
+#define cd(condition, value_if_true, value_if_false) ((condition) ? (value_if_true) : (value_if_false))
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+const int inf = 9e17;
+const int mod = 1e9 + 7;
+const int NUM = 1000030;
+const int N = 10000000;
+const int MAX_N = 1e6 + 5;
+
+#pragma GCC optimize("unroll-loops,O3,Ofast")
+#pragma GCC target("avx2,avx,fma,bmi,bmi2,lzcnt,popcnt")
+
+// Global matrix to prevent stack overflow
+// const lli MAX_N = 1000; // Adjust based on constraints
+// vector<vector<lli>> a(MAX_N, vector<lli>(MAX_N));
+
+lli dp[1000100];
+void neepan()
+{
+    lli n, k;
+    cin >> n >> k;
+    lli a[k];
+    fr(i, 0, k - 1) cin >> a[i];
+    fr(i, 0, n)
+    {
+        if (dp[i] == 0)
+        {
+            fr(j, 0, k - 1)
+            {
+                if (i + a[j] <= n)
+                {
+                    dp[i + a[j]] = 1;
+                }
+            }
+        }
+        if (i)
+            cout << (dp[i] ? 'W' : 'L');
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    _test
+    neepan();
+}
