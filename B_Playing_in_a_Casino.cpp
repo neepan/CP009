155eb4a5accc3ac5a6a4605421d45d8745b31317
neepan <neepanbiswas@gmail.com>
2025-05-10 12:00:21 +0530

    adde

---
+++ B_Playing_in_a_Casino.cpp
@@ -0,0 +1,91 @@
+// Neepan Biswas
+// neepan ~ codeforces
+#include <bits/stdc++.h>
+using namespace std;
+
+using lli = long long int;
+using ld = long double;
+using pi = pair<lli, lli>;
+using vp = vector<pi>;
+using vi = vector<lli>;
+using mp = map<lli, lli>;
+using ump = unordered_map<lli, lli>;
+
+#define fr(i, x, n) for (lli i = (x); i <= (n); i++)
+#define fr1(j, n, x) for (lli j = (n); j >= (x); j--)
+#define _test      \
+    int _TEST = 1; \
+    cin >> _TEST;  \
+    while (_TEST--)
+#define pb push_back
+#define eb emplace_back
+#define sz(x) (lli) x.size()
+#define ff first
+#define ss second
+#define all(v) v.begin(), v.end()
+#define debug(x) cout << '>' << #x << ':' << x << endl;
+#define cd(condition, value_if_true, value_if_false) ((condition) ? (value_if_true) : (value_if_false))
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+const int inf = 9e17;
+const int mod = 1e9 + 7;
+const int NUM = 1000030;
+const int N = 10000000;
+const int MAX_N = 1e6 + 5;
+
+#pragma GCC optimize("unroll-loops,O3,Ofast")
+#pragma GCC target("avx2,avx,fma,bmi,bmi2,lzcnt,popcnt")
+
+// Global matrix to prevent stack overflow
+// const lli MAX_N = 1000; // Adjust based on constraints
+// vector<vector<lli>> a(MAX_N, vector<lli>(MAX_N));
+
+void neepan()
+{
+    lli n, m;
+    cin >> n >> m;
+    vector<vi> v(m);
+
+    // taking input column wise
+    fr(i, 0, n - 1)
+    {
+        fr(j, 0, m - 1)
+        {
+            lli x;
+            cin >> x;
+            v[j].pb(x);
+        }
+    }
+    fr(i,0,m-1){
+        sort(v[i].begin(),v[i].end());
+    }
+    lli ans = 0;
+    fr(i, 0, m - 1)
+    {
+        vi res(n, 0);
+        res[n - 1] = v[i][n - 1];
+        fr1(j, n - 2, 0)
+        {
+            res[j] = res[j + 1] + v[i][j];// storing suffix sum that is from the end to first
+        }
+
+        // 
+        fr(j, 0, n - 2)
+        {
+            lli cnt = res[j + 1] - (n - 1 - j) * v[i][j];
+            ans += cnt;
+        }
+    }
+
+    cout << ans << endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    _test
+    neepan();
+}
