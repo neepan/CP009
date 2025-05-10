63874058fa4bc35a3bf5d506c35ddd9eb104f42a
neepan <neepanbiswas@gmail.com>
2025-05-10 15:30:23 +0530

    added

---
+++ C_Dora_and_Search.cpp
@@ -0,0 +1,99 @@
+// Neepan Biswas
+// neepan ~ codeforces
+#include <bits/stdc++.h>
+using namespace std;
+
+using lli = long long int;
+using ld = long double;
+using pi = pair<lli, lli>;
+using vp = vector<pi>;
+using vi = vector<lli>;
+using mp = map<lli, lli>;
+using ump = unordered_map<lli, lli>;
+
+#define fr(i, x, n) for (lli i = (x); i <= (n); i++)
+#define fr1(j, n, x) for (lli j = (n); j >= (x); j--)
+#define _test      \
+    int _TEST = 1; \
+    cin >> _TEST;  \
+    while (_TEST--)
+#define pb push_back
+#define eb emplace_back
+#define sz(x) (lli) x.size()
+#define ff first
+#define ss second
+#define all(v) v.begin(), v.end()
+#define debug(x) cout << '>' << #x << ':' << x << endl;
+#define cd(condition, value_if_true, value_if_false) ((condition) ? (value_if_true) : (value_if_false))
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+const int inf = 9e17;
+const int mod = 1e9 + 7;
+const int NUM = 1000030;
+const int N = 10000000;
+const int MAX_N = 1e6 + 5;
+
+#pragma GCC optimize("unroll-loops,O3,Ofast")
+#pragma GCC target("avx2,avx,fma,bmi,bmi2,lzcnt,popcnt")
+
+// Global matrix to prevent stack overflow
+// const lli MAX_N = 1000; // Adjust based on constraints
+// vector<vector<lli>> a(MAX_N, vector<lli>(MAX_N));
+
+void neepan()
+{
+    lli n;
+    cin >> n;
+    vi a(n);
+    fr(i, 0, n - 1) cin >> a[i];
+
+    lli l = 0, h = n - 1, mn = 1, mx = n;
+    lli check = 0;
+    while (h >= l)
+    {
+        if (a[l] == 1 && a[h] == n || a[h] == 1 && a[l] == n)
+        {
+            l++;
+            h--;
+            mn++;
+            mx--;
+        }
+        else if (a[h] == mx)
+        {
+            h--;
+            mx--;
+        }
+        else if (a[h] == mn)
+        {
+            h--;
+            mn++;
+        }
+        else if (a[l] == mx)
+        {
+            l++;
+            mx--;
+        }
+        else if (a[l] == mn)
+        {
+            l++;
+            mn++;
+        }
+        else
+        {
+            cout << l + 1 << " " << h + 1 << endl;
+            return;
+        }
+    }
+
+    cout << -1 << endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    _test
+    neepan();
+}
