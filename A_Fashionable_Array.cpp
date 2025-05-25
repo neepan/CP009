36090ce406c2ef69e4fe61a1b556bd78372b69fc
neepan <neepanbiswas@gmail.com>
2025-05-25 12:14:13 +0530

    added

---
+++ A_Fashionable_Array.cpp
@@ -0,0 +1,84 @@
+// Neepan Biswas
+// neepan ~ codeforces
+#include <bits/stdc++.h>
+using namespace std;
+
+using lli = long long int;
+using ld = long double;
+using pi = pair<lli, lli>;
+using vp = vector<pi>;
+using vi = vector<lli>;
+using mp = map<lli, lli>;
+using ump = unordered_map<lli, lli>;
+
+#define fr(i, x, n) for (lli i = (x); i <= (n); i++)
+#define fr1(j, n, x) for (lli j = (n); j >= (x); j--)
+#define _test      \
+    int _TEST = 1; \
+    cin >> _TEST;  \
+    while (_TEST--)
+#define pb push_back
+#define eb emplace_back
+#define sz(x) (lli) x.size()
+#define ff first
+#define ss second
+#define all(v) v.begin(), v.end()
+#define rev(v) v.rbegin(), v.rend()
+#define debug(x) cout << '>' << #x << ':' << x << endl;
+#define cd(condition, value_if_true, value_if_false) ((condition) ? (value_if_true) : (value_if_false))
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+const lli inf = 9e17;
+const int mod = 1e9 + 7;
+const int NUM = 1000030;
+const int N = 10000000;
+const int MAX_N = 1e6 + 5;
+
+#pragma GCC optimize("unroll-loops,O3,Ofast")
+#pragma GCC target("avx2,avx,fma,bmi,bmi2,lzcnt,popcnt")
+
+// Global matrix to prevent stack overflow
+// const lli MAX_N = 1000; // Adjust based on constraints
+// vector<vector<lli>> a(MAX_N, vector<lli>(MAX_N));
+
+void neepan()
+{
+    lli n;
+    cin >> n;
+    vi a(n);
+    fr(i, 0, n - 1) cin >> a[i];
+
+    sort(all(a));
+    if (!((a[0] + a[n - 1]) & 1))
+    {
+        cout << 0 << endl;
+        return;
+    }
+    lli mn = 0;
+    fr(i, 0, n - 1)
+    {
+        if ((a[i] + a[n - 1]) & 1)
+            mn++;
+        else
+            break;
+    }
+    lli mx = 0;
+    fr1(i, n - 1, 0)
+    {
+        if ((a[i] + a[0]) & 1)
+            mx++;
+        else
+            break;
+    }
+    cout << min(mn, mx) << endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    _test
+    neepan();
+}
