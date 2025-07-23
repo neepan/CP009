8f8959081e04d775dd99aebda8c6271f2b497fa3
neepan <neepanbiswas@gmail.com>
2025-07-23 10:26:55 +0530

    added

--- B_Left_and_Down.cpp
+++ B_Left_and_Down.cpp
@@ -1,67 +1,72 @@
 // Neepan Biswas
 // neepan ~ codeforces
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
+ 
 using lli = long long int;
 using ld = long double;
-using pi = pair<lli, lli>;
+using pi = pair<lli,lli>;
 using vp = vector<pi>;
 using vi = vector<lli>;
-using mp = map<lli, lli>;
-using ump = unordered_map<lli, lli>;
-
-#define fr(i, x, n) for (lli i = (x); i <= (n); i++)
-#define fr1(j, n, x) for (lli j = (n); j >= (x); j--)
-#define _test      \
-    int _TEST = 1; \
-    cin >> _TEST;  \
-    while (_TEST--)
+using mp=map<lli,lli>;
+using ump=unordered_map<lli,lli>;
+ 
+#define fr(i,x,n) for (lli i=(x);i<=(n);i++)
+#define fr1(j, n,x) for (lli j = (n); j >= (x); j--)
+#define _test   int _TEST=1; cin>>_TEST; while(_TEST--)
 #define pb push_back
 #define eb emplace_back
-#define sz(x) (lli) x.size()
+#define sz(x) (lli)x.size()
 #define ff first
 #define ss second
 #define all(v) v.begin(), v.end()
 #define rev(v) v.rbegin(), v.rend()
 #define debug(x) cout << '>' << #x << ':' << x << endl;
 #define cd(condition, value_if_true, value_if_false) ((condition) ? (value_if_true) : (value_if_false))
-#define yes cout << "YES" << endl
-#define no cout << "NO" << endl
-
+#define yes cout << "YES"<<endl
+#define no cout << "NO"<<endl
+ 
 const lli inf = 9e17;
 const int mod = 1e9 + 7;
 const int NUM = 1000030;
 const int N = 10000000;
 const int MAX_N = 1e6 + 5;
-
+ 
 #pragma GCC optimize("unroll-loops,O3,Ofast")
 #pragma GCC target("avx2,avx,fma,bmi,bmi2,lzcnt,popcnt")
-
+ 
 // Global matrix to prevent stack overflow
 // const lli MAX_N = 1000; // Adjust based on constraints
 // vector<vector<lli>> a(MAX_N, vector<lli>(MAX_N));
+ 
 
-
-void neepan()
-{
-    string s;
-    cin >> s;
-
-   
-    sort(all(s));
-    reverse(all(s));
-
-    cout<<s<<endl;
-    
-    
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
 }
 
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+void neepan(){
+lli a,b,k;
+cin>>a>>b>>k;
+    lli g=gcd(a,b);
+    lli dx=a/g, dy=b/g;
+    if(dx<=k && dy<=k){
+        cout<<1<<endl;
+    }
+    else{
+        cout<<2<<endl;
+    }
 
-    _test
-    neepan();
+}
+ 
+int main(){
+ios::sync_with_stdio(false);
+cin.tie(nullptr);
+ 
+_test
+neepan();
 }
